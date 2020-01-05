5da16d4a96b9b9a549e77b56a8912292cd10a110
Estelle Choi <estele.choi@gmail.com>
2020-01-05 17:08:04 +0900

    Write a program that prints a staircase of size

--- hackerrank/2.js
+++ hackerrank/2.js
@@ -1,5 +1,6 @@
 // Observe that its base and height are both equal to , and the image is drawn using # symbols and spaces.
 // The last line is not preceded by any spaces.
+
 // Write a program that prints a staircase of size .
 
 'use strict';
