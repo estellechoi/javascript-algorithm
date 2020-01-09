6b8e560c9941ec5ee4a18a3899dc18b2ecb7cdc8
Estelle Choi <estele.choi@gmail.com>
2020-01-09 15:53:26 +0900

    Migratory Birds / Array.sort(); 메소드를 몰라서 벌어진 일

--- hackerrank/13.js
+++ hackerrank/13.js
@@ -1,7 +1,10 @@
+// You have been asked to help study the population of birds migrating across the continent.
+// Each type of bird you are interested in will be identified by an integer value.
 // Each time a particular kind of bird is spotted, its id number will be added to your array of sightings.
 // You would like to be able to find out which type of bird is most common given a list of sightings.
 // Your task is to print the type number of that bird and
 // if two or more types of birds are equally common, choose the type with the smallest ID number.
+
 'use strict';
 
 const fs = require('fs');
@@ -28,50 +31,35 @@ function readLine() {
 
 // Complete the migratoryBirds function below.
 function migratoryBirds(arr) {
-	let types = [];
-	let max = 0;
-	arr.forEach(function (bird, index) {
-		if (index === 0) {
-			types.push({
-				id: bird,
-				number: 1
-			}); // add as a new
-			max = 1;
-			return;
-		}
-		types.forEach(function (type) {
-			if (type.id === bird) {
-				type.number += 1;
-				if (type.number > max) {
-					max = type.number;
-				}
-			} else {
-				types.push({
-					id: bird,
-					number: 1
-				}); // add as a new
+	// arr.sort();
+	// 오름차순, ASCII 문자 순서
+	// ASCII 문자 순서로 정렬되어 숫자의 크기대로 나오지 않음
+	// arr.sort().reverse(); 내림차순
+
+	// 숫자 오름차순
+	arr.sort(function (a, b) {
+		// 반환값 (+, -, 0)에 따라 위치 결정
+		// 반환값 (a - b)이 음수이면, a < b 라고 판단하여 a -> b 순으로 정렬시킨다.
+		return a - b;
+	});
+
+	let max = 1;
+	let count = 1;
+	let result = 0;
+	arr.forEach(function (id, i) {
+		if (i === 0) return;
+		if (id === arr[i - 1]) {
+			count++;
+			if (count > max) {
+				max = count;
+				result = id;
 			}
-		});
-	});
-	var ids = [];
-	types.forEach(function (type) {
-		if (type.number === max) {
-			ids.push(type.id);
+		} else {
+			count = 1;
 		}
 	});
 
-	var idMin = 0;
-	ids.forEach(function (id, index) {
-		if (index === 0) {
-			idMin = ids[0];
-			return;
-		}
-		if (id < idMin) {
-			idMin = id;
-		}
-	});
-
-	return idMin;
+	return result;
 }
 
 function main() {
